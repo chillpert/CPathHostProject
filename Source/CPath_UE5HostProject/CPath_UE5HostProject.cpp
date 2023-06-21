16656ac46e88ecb2abbcee3c48ddc20ca4c75a1c
chillpert <dev@christianhilpert.com>
2023-06-21 16:29:11 +0200

    Remove duplicate files

--- Source/CPath_UE5HostProject/CPath_UE5HostProject.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPath_UE5HostProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPath_UE5HostProject, "CPath_UE5HostProject" );
- 
